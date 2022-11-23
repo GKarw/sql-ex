46c74d2299f3d1077c8705b7af0e4fc2c29d77ee
GKarw <100209962+GKarw@users.noreply.github.com>
2022-11-23 15:28:26 +0100

    Create 35.sql

---
+++ 35.sql
@@ -0,0 +1,6 @@
+-- Find models in the Product table consisting either of digits only or Latin letters (A-Z, case insensitive) only.
+-- Result set: model, type.
+
+SELECT model, type
+FROM Product
+WHERE model NOT LIKE '%[^0-9]%' OR model NOT LIKE '%[^A-Z]%'
